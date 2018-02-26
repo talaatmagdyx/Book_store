7c4f9e7ba697dba56199c9b15bf15a504a3b137e
TalaatMagdy <talaatmagdy75@gmail.com>
2018-02-26 19:45:08 +0200

    add product

---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+# . . .
+Product.create(title: 'java',
+description:
+%{<p>
+<em>Native Apps, Multiple Platforms</em>
+Answer the question “Can we build this for ALL the devices?” with a
+resounding YES. This book will help you get there with a real-world
+introduction to seven platforms, whether you’re new to mobile or an
+experienced developer needing to expand your options. Plus, you’ll find
+out which cross-platform solution makes the most sense for your needs.
+</p>},
+image_url: 'java.png',
+price: 26.00)
+# . . .
---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+setup do
+  @product = products(:one)
+   @update = {
+   title: 'Lorem Ipsum',
+   description: 'Wibbles are fun!',
+   image_url: 'java.png',
+   price: 19.95
+ }
+end
+test "should get index" do
+get products_url
+assert_response :success
+end
+test "should get new" do
+get new_product_url
+assert_response :success
+end
+test "should create product" do
+assert_difference('Product.count') do
+ post products_url, params: { product: @update }
+end
+assert_redirected_to product_url(Product.last)
+end
+# ...
+test "should update product" do
+ patch product_url(@product), params: { product: @update }
+assert_redirected_to product_url(@product)
+end
+# ...
+end

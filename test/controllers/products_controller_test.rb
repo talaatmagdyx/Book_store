472b02a64e6631499e1506be69aa900f96903d22
TalaatMagdy <talaatmagdy75@gmail.com>
2018-03-03 20:18:46 +0200

    add button cart

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -29,4 +29,17 @@ test "should update product" do
 assert_redirected_to product_url(@product)
 end
 # ...
+test "can't delete product in cart" do
+ assert_difference('Product.count', 0) do
+ delete product_url(products(:two))
+ end
+
+ assert_redirected_to products_url
+ end
+test "should destroy product" do
+assert_difference('Product.count', -1) do
+delete product_url(@product)
+end
+assert_redirected_to products_url
+end
 end
